17da22e8f57827a52cd3d5654309df697e4de640
doyo91 <doyo_91@hotmail.com>
2020-11-12 21:10:22 +0100

    touchable components delete

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -13,22 +19,24 @@ export default function App() {
     { name: "Adri", id: "8" },
   ]);
 
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => person.id != id);
+    });
+  };
   return (
     <View style={styles.container}>
       <FlatList
         numColumns={2}
         keyExtractor={(item) => item.id}
         data={people}
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-      />
-
-      {/* <ScrollView>
-        {people.map((item) => (
-          <View key={item.key}>
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
             <Text style={styles.item}>{item.name}</Text>
-          </View>
-        ))}
-      </ScrollView> */}
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
@@ -49,6 +57,5 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginHorizontal: 10,
     marginTop: 24,
-    flex: 1,
   },
 });
