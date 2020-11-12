9d30cb31a9f66ad24b34d6769af83c0f86ce9054
doyo91 <doyo_91@hotmail.com>
2020-11-12 20:14:12 +0100

    onPress inline

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Samuel");
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>
-          Lorem ipsum <Text>Test</Text> dolor sit amet.
-        </Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
+      <Text>My name is {name}</Text>
+      <Text></Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={() => setName("doyo")} />
       </View>
     </View>
   );
@@ -25,15 +21,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  header: {
-    backgroundColor: "pink",
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: "bold",
-  },
-  body: {
-    backgroundColor: "yellow",
-    padding: 20,
+  buttonContainer: {
+    marginTop: 20,
   },
 });
