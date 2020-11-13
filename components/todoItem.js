8791ce51d9fe7ce0bc043a11bf8dcf5569b2a951
doyo91 <doyo_91@hotmail.com>
2020-11-13 08:27:39 +0100

    icon

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,21 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export const TodoItem = ({ item, pressHandler }) => {
   return (
-    <TouchableOpacity onPress={() => pressHandler(item.key)}>
+    <View style={styles.todoContainer}>
+      <TouchableOpacity onPress={() => pressHandler(item.key)}>
+        <MaterialIcons style={styles.delete} name="delete" />
+      </TouchableOpacity>
       <Text style={styles.text}>{item.text}</Text>
-    </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
+  todoContainer: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
     padding: 16,
     marginTop: 16,
     borderColor: "#bbb",
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+  },
+  text: {
+    paddingRight: 10,
+  },
+  delete: {
+    padding: 10,
+    color: "coral",
+    fontSize: 22,
   },
 });
