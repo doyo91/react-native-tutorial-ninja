82137ef45df24e76095333d60b129d95340e1a19
doyo91 <doyo_91@hotmail.com>
2020-11-13 08:32:11 +0100

    alerts

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, View, Alert } from "react-native";
 import { AddTodo } from "./components/addTodo";
 import { Header } from "./components/header";
 import { TodoItem } from "./components/todoItem";
@@ -18,9 +18,15 @@ export default function App() {
   };
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [{ text: text, key: Math.random().toString() }, ...prevTodos];
-    });
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("OOPS!", "Todos must be over 3 chars long", [
+        { text: "Understood", onPress: () => console.log("alert closed") },
+      ]);
+    }
   };
 
   return (
